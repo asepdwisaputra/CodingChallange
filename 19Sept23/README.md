4a1a6b9dec13b97f883c9962572a33e15b36b5f3
asepdwisaputra <akunasep111@gmail.com>
2023-09-19 12:47:03 +0700

    Add: CC 19Sept23

---
+++ 19Sept23/README.md
@@ -0,0 +1,7 @@
+# 19 – 24 September 2023
+## Basic
+## Advance
+I am writing a program to calculate, what is the total number of a sequence. where this sequence has the pattern '2-22-222-...(sequence of number 2 followed by digit "2" n-1 times)'.
+
+# Questions
+https://www.smk.dev/challenge/7644/?utm_source=whatsapp&utm_medium=social&utm_campaign=coding-challenge
---
+++ 19Sept23/advance.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func TotalNilai(angka int) int {
+	// Saya berencana memasukan nilai dari urutan ke sebuah temporary slice
+	wadah := []string{}
+
+	// Disini saya ingin mengkonversi input parameter ke bentuk 2, 22, 222, ...
+	var nilai strings.Builder
+	for angka > 0 {
+		nilai.WriteString("2")
+		wadah = append(wadah, nilai.String()) // memasukan ke wadah
+		angka--
+	}
+
+	// Disini saya ingin memecah setiap wadah tadi
+	// Kemudian didalamnya ada konversi string>>int
+	// Kemudian Jumlahkan setiap isinya
+	var result int
+	for _, isi := range wadah {
+		nilai, _ := strconv.Atoi(isi)
+		result = result + nilai
+	}
+	return result
+}
+
+func main() {
+	var input int
+	fmt.Print("Masukan Jumlah Angka Urutan: ")
+	fmt.Scanln(&input)
+
+	if input <= 0 {
+		fmt.Println("Masukan Urutan lebih dari 0!")
+	}
+
+	hasil := TotalNilai(input)
+
+	fmt.Printf("Jumlah nilai urutan kamu adalah: %d", hasil)
+}
